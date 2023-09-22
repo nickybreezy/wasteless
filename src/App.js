db4aba55d18e3b28c947c22ee27026fff1b9561d
Nicky <n.tabrizifar@student.fontys.nl>
2023-09-22 12:55:30 +0200

    added scan button AND bottombar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,61 @@
-import './App.css';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-function App() {
+import productDataJson from './productData.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { BiScan } from 'react-icons/bi'; // Import the Amazon icon from react-icons library
 
+function App() {
   const [productData, setProductData] = useState({});
-  const barcode = '123456789';
+  const [scannedBarcode, setScannedBarcode] = useState('');
+  const [scanning, setScanning] = useState(false);
 
   useEffect(() => {
-    axios.get('/productData.json')
-      .then((response) => {
-        if (response.status === 200) {
-          setProductData(response.data);
-        } else {
-          console.error('Failed to fetch product data');
-        }
-      })
-      .catch((error) => {
-        console.error('Error fetching product data:', error);
-      });
-  }, []);
+    if (scanning) {
+      const data = productDataJson[scannedBarcode];
+
+      if (data) {
+        setProductData(data);
+      } else {
+        console.error('Product niet gevonden');
+      }
+
+      setScanning(false);
+    }
+  }, [scannedBarcode, scanning]);
+
+  const handleScan = (barcode) => {
+    setScannedBarcode(barcode);
+    setScanning(true);
+  };
 
   return (
-    <div>
-      <h2>Productinformatie</h2>
-      <p>Naam: {productData[barcode]?.name}</p>
-      <p>Materiaal: {productData[barcode]?.material}</p>
-      {productData[barcode]?.recyclable ? (
-        <p>Dit product is recyclebaar.</p>
-      ) : (
-        <p>Dit product is niet recyclebaar.</p>
-      )}
+    <div className="app-container">
+      <div className="custom-bg">
+        <div>
+          <h2>Productinformatie</h2>
+          <p>Gescande barcode: {scannedBarcode}</p>
+          <p>Naam: {productData.name}</p>
+          <p>Materiaal: {productData.material}</p>
+
+          {productData.recyclable ? (
+            <p>Dit product is recyclebaar.</p>
+          ) : (
+            <p>Dit product is niet recyclebaar.</p>
+          )}
+
+        </div>
+        <div className="bottom-bar">
+          <button className="btn btn-solid" onClick={() => handleScan('456789123')}>
+            <BiScan style={{ color: 'white' }} />
+          </button>
+
+        </div>
+      </div>
+
+
+
     </div>
+
   );
 }
 
